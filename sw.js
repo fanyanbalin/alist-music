054331226309b8004d1dc48149c63f2a4ed170a1
fanyanbalin <fanyanbalin@gmail.com>
2026-06-10 21:39:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,51 @@
-self.addEventListener('install', function() {
+const CACHE_VERSION = 'dmusic-static-v1';
+const STATIC_ASSETS = [
+	'./',
+	'./index.html',
+	'./alist.js',
+	'./manifest.json',
+	'./favicon.ico'
+];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(caches.open(CACHE_VERSION).then(function(cache) {
+		return cache.addAll(STATIC_ASSETS);
+	}));
 	self.skipWaiting();
 });
 
 self.addEventListener('activate', function(event) {
-	event.waitUntil(self.clients.claim());
+	event.waitUntil(Promise.all([
+		caches.keys().then(function(keys) {
+			return Promise.all(keys.filter(function(key) {
+				return key !== CACHE_VERSION;
+			}).map(function(key) {
+				return caches.delete(key);
+			}));
+		}),
+		self.clients.claim()
+	]));
 });
 
 self.addEventListener('fetch', function(event) {
-	event.respondWith(fetch(event.request));
+	if (event.request.method !== 'GET') return;
+	const requestUrl = new URL(event.request.url);
+	if (requestUrl.origin !== self.location.origin) return;
+
+	event.respondWith(caches.match(event.request).then(function(cachedResponse) {
+		if (cachedResponse) return cachedResponse;
+		return fetch(event.request).then(function(networkResponse) {
+			if (!networkResponse || networkResponse.status !== 200) return networkResponse;
+			const responseToCache = networkResponse.clone();
+			caches.open(CACHE_VERSION).then(function(cache) {
+				cache.put(event.request, responseToCache);
+			});
+			return networkResponse;
+		}).catch(function() {
+			if (event.request.mode === 'navigate') {
+				return caches.match('./index.html');
+			}
+			return caches.match(event.request);
+		});
+	}));
 });
